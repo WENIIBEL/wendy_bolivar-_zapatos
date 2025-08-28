a16ae7de2603d03969a9d309e9d94fdee12f30a3
WENIIBEL <salak403@salasfr.itm.edu.co>
2025-08-28 07:11:13 -0500

    proyecto clase zapato1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+class Zapatos
+{
+    int talla;
+    String nombre;
+    String color;
+    int precio
+}
